121f0efbed03f494285a57b2879585cb7d9a63aa
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-02 00:39:57 +0000

    ejercicio 27

--- exercises/27-computeAreaOfATriangle/app.js
+++ exercises/27-computeAreaOfATriangle/app.js
@@ -1,2 +1,9 @@
 // Write your function here
+function computeAreaOfATriangle(num , num2){
+    return (num*num2) / 2
+
+}
+
+var output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
 
