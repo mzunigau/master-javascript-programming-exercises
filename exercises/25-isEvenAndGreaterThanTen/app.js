03e1b2cdef2ec55455959de77d362974114c7a17
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-03-02 00:27:20 +0000

    ejercicio 25

--- exercises/25-isEvenAndGreaterThanTen/app.js
+++ exercises/25-isEvenAndGreaterThanTen/app.js
@@ -1,1 +1,13 @@
 // Write your function here
+function isEvenAndGreaterThanTen(num){
+    if(num > 10 && num%2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+var output = isEvenAndGreaterThanTen(13);
+console.log(output); // --> falso
